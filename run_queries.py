321e0f79cecff2430da0e0d90d552c77d815c9fb
LumingSun <sunluming@ruc.edu.cn>
2021-11-23 11:16:17 +0800

    workload run

--- run_queries.py
+++ run_queries.py
@@ -4,7 +4,8 @@ import sys
 import random
 from time import time, sleep
 
-USE_BAO = True
+#USE_BAO = True
+USE_BAO = False
 PG_CONNECTION_STR = "dbname=imdb user=imdb host=localhost"
 
 # https://stackoverflow.com/questions/312443/
